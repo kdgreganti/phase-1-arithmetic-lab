022fda86607133c38ed12c8f62ec735314535d71
Kyle Greganti <kyle.greganti@yahoo.com>
2024-01-03 20:08:16 -0600

    meets requirements.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = (num1 * num2);
+console.log(multiply);
+
+
+
+
+ let random = Math.floor(Math.random() * 10) + 1;
+ console.log(random);
+
+
+let num3 = 9
+let num4 = 40
+
+let mod = (num4 % num3);
+console.log(mod);
+  
+ 
+let max = Math.max(1, 4, 8, 12, 20);
+
+console.log(max);
